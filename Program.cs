0798e983400e722507fcddf3877af7103d088e42
Mikael Pajari <mikael.pajari@live.se>
2020-09-16 13:50:01 +0200

    klar

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ne744y
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int x = 100;
+           int sum = 0;
+            for(int i = 0; i < x; i++)
+            {
+            sum += i;
+            Console.WriteLine(sum);
+            }
+            
+
+            
+
+        }
+    }
+}
